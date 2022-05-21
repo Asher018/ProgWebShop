1c6269e879a74fd963010a4d805b0d9f8f39575a
Kovács Marcell <h983855@stud.u-szeged.hu>
2022-05-21 17:20:47 +0200

    offline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFirestore, ENABLE_PERSISTENCE } from '@angular/fire/compat/firestore';
-import { enableIndexedDbPersistence } from "firebase/firestore";
-import * as firebase from 'firebase/compat';
-import { Observable, Subject } from 'rxjs';
-import { startWith, switchMap } from "rxjs/operators";
-import { Item } from './shared/models/item.model';
-import { DatabaseService } from './shared/services/database.service';
+import { getStorage } from 'firebase/storage';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +10,9 @@ export class AppComponent {
   title = 'ProgWebShop';
 
 
-  constructor(private firestore:AngularFirestore) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.firestore.persistenceEnabled$.subscribe()  
-    enableIndexedDbPersistence
-    ENABLE_PERSISTENCE
   }
 
 
